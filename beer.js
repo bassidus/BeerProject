530356d93c62a2deda74598e1e7686e7490f509f
Carl-Magnus Arvidsson <63563704+bassidus@users.noreply.github.com>
2020-12-08 19:06:16 +0100

    fixes

--- beer.js
+++ beer.js
@@ -1,45 +1,93 @@
 // https://github.com/zocom-david-lundholm/js10-vanilla-slutprojekt
 
-const root = 'https://api.punkapi.com/v2/beers';
-const random = 'https://api.punkapi.com/v2/beers/random';
-const beername = '?beer_name=';
 const displaySection = document.querySelector('main > div.display');
 const searchInp = document.querySelector('.flexdiv > input.inp');
 const searchBtn = document.querySelector('.flexdiv > input.search');
+const rndBtn = document.querySelector('input.rnd');
 const advBtn = document.querySelector('.flexdiv > input.adv');
 const advSection = document.querySelector('main > div.advanced');
-const advName = document.querySelector('input[name=name]');
-const advHops = document.querySelector('input[name=hops]');
-const advMalt = document.querySelector('input[name=malt]');
-const advBrewBef = document.querySelector('input[name=brewbefore]');
-const advBrewAft = document.querySelector('input[name=brewafter]');
-const advAbvGre = document.querySelector('input[name=abvgreater]');
-const advAbvLes = document.querySelector('input[name=abvless]');
+const advInputs = document.querySelectorAll('form > input[type=text]');
 const advSubmit = document.querySelector('form > input[type=button]');
-const resultUl = document.querySelector('.results > ul');
 
-advSubmit.addEventListener('click', (e) => {
-    console.log(e)
-    getData(`${root}${beername}${advName.value}`, advSearch);
+const root = 'https://api.punkapi.com/v2/beers';
+const random = 'https://api.punkapi.com/v2/beers/random';
+const beername = '?beer_name=';
+const latestResults = [];
+
+rndBtn.addEventListener('click', () => getData(random, renderBeer));
+advInputs.forEach(input => {
+    input.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') {
+            advSearch();
+        }
+    });
 });
+advSubmit.addEventListener('click', advSearch);
+function validate() {
+    // validera advInputs
+    return true;
+}
+function advSearch() {
+    if (validate() == false) return;
 
-function advSearch(result) {
-    let name = '?beer_name=' + advName.value;
-    let abv_gt = '?abv_gt=' + advAbvGre.value;
-    let abv_lt = '?abv_lt=' + advAbvLes.value;
-    let hops = '?hops=' + advHops.value;
-    let malt = '?malt=' + advMalt.value;
-    let brewed_before = '?brewed_before=' + advBrewBef.value;
-    let brewed_after = '?brewed_after=' + advBrewAft.value;
-    
+    let searchString = root;
+    const parameters = ['?beer_name=', '?hops=', '?malt=', '?brewed_before=', '?brewed_after=', '?abv_gt=', '?abv_lt=']
+
+    for (let i = 0; i < parameters.length; i++) {
+        const par = parameters[i];
+        const inp = advInputs[i];
+        searchString += inp.value.length > 0 ? par + inp.value : '';
+    }
+    getData(`${searchString}`, cacheAdvResults);
+}
+
+function cacheAdvResults(result) {
+    latestResults.length = 0;
     console.log(result)
-    removeAllChildNodes(resultUl);
     result.forEach(beer => {
+        latestResults.push(beer);
+    });
+
+    renderPageButtons(latestResults.length);
+    getPage(0);
+}
+
+function getPage(start) {
+    const advResultsSection = document.querySelector('.advresults');
+    removeAllChildNodes(advResultsSection);
+
+    const advresultDiv = document.createElement('div');
+    const resultUl = document.createElement('ul');
+
+    for (let i = start; i < (start + 10); i++) {
+        const beer = latestResults[i];
+        if (beer == undefined) break;
         const li = document.createElement('li');
         li.textContent = beer.name;
         resultUl.appendChild(li);
-    });
+    }
+
+    advresultDiv.appendChild(resultUl);
+    advResultsSection.appendChild(advresultDiv);
 }
+
+function renderPageButtons(length) {
+    const pageButtonsSection = document.querySelector('.pageButtonsSection');
+    removeAllChildNodes(pageButtonsSection);
+
+    let numPages = Math.ceil(length / 10);
+    for (let i = 0; i < numPages; i++) {
+        const pageBtn = document.createElement('button');
+        pageBtn.textContent = i + 1;
+        pageBtn.addEventListener('click', () => {
+            getPage(i * 10);
+        });
+        pageButtonsSection.appendChild(pageBtn);
+    }
+    const results = document.querySelector('.results h2');
+    results.textContent = `Search Results (${length})`;
+}
+
 
 searchInp.addEventListener('keydown', (e) => {
     if (e.key == 'Enter') {
@@ -57,8 +105,7 @@ advBtn.addEventListener('click', () => {
 });
 
 
-const rndBtn = document.querySelector('input.rnd');
-rndBtn.addEventListener('click', () => getData(random, renderBeer));
+
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -93,13 +140,11 @@ function renderBeer(data) {
 
         h4Div.appendChild(h4);
         outerDiv.appendChild(h4Div)
-
         outerDiv.appendChild(p);
 
         imgDiv.classList.add('imgcontainer');
         outerDiv.classList.add('beercontainer');
         h4Div.classList.add('title')
-
 
         h4.textContent = beer.name;
         p.textContent = 'See More >';
